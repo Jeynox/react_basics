e200572d156b8e3ce78eebcf61e05bb51020c506
Jeynox <daniel.lemos.jose1998@gmail.com>
2025-01-14 12:32:31 +0100

    ajout de la quete 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import "./App.css";
 import CardPokemon from "./components/CardPokemon";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
   return (
     <div>
-      <CardPokemon />
+      <CardPokemon pokemon={
+        pokemonList[0]
+      } />
     </div>
   );
 }
