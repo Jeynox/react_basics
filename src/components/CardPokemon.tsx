90ced1800e76c9ef35a94c07e8b01683a45c8283
Jeynox <daniel.lemos.jose1998@gmail.com>
2025-01-14 12:18:50 +0100

    ajout de la quete 3

--- src/components/CardPokemon.tsx
+++ src/components/CardPokemon.tsx
@@ -1,9 +1,22 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function CardPokemon() {
+    const pokemon = pokemonList[0];
+
     return (
         <>
             <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-                <figcaption>Bulbasaur</figcaption>
+                {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt="" /> : <p>???</p>}
+                <figcaption>{pokemon.name}</figcaption>
             </figure>
         </>
     );
